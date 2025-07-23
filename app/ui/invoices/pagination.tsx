7905435d2f6bcb5b0f15ec059383732fdf80ca01
eziz-tajiev <eziztajiev@gmail.com>
2025-07-23 13:01:44 +0500

    remove uncorrect import

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -5,7 +5,6 @@ import clsx from "clsx";
 import Link from "next/link";
 import { generatePagination } from "@/app/lib/utils";
 import { usePathname, useSearchParams } from "next/navigation";
-import { use } from "react";
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: Uncomment this code in Chapter 11
